b93ec0a6c3bb4060b010daf9c6ea2e56fe0a4e14
tomasdarquier <tomasdarquier@gmail.com>
2024-10-11 14:15:30 -0300

    Implementacion de seguridad con Auth0 y OAuth2

---
+++ gateway/src/main/java/com/example/gateway/configuration/SecurityConfiguration.java
@@ -0,0 +1,75 @@
+package com.example.gateway.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import java.util.function.Consumer;
+
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Value("${okta.oauth2.audience}")
+    private String audience;
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfiguration(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    private LogoutSuccessHandler logoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler logoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+
+        // Hacia donde se redirigira al usuario despues del logout
+        logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+
+        return logoutSuccessHandler;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2
+                        .authorizationEndpoint(authorization -> authorization
+                                .authorizationRequestResolver(
+                                        authorizationRequestResolver(this.clientRegistrationRepository)
+                                )
+                        )
+                )
+                .oauth2ResourceServer(jwt -> jwt.jwt(withDefaults()))
+                .logout(logout -> logout.logoutSuccessHandler(logoutSuccessHandler()));
+        return http.build();
+    }
+
+    private OAuth2AuthorizationRequestResolver authorizationRequestResolver(
+            ClientRegistrationRepository clientRegistrationRepository) {
+
+        DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver =
+                new DefaultOAuth2AuthorizationRequestResolver(
+                        clientRegistrationRepository, "/oauth2/authorization");
+        authorizationRequestResolver.setAuthorizationRequestCustomizer(
+                authorizationRequestCustomizer());
+
+        return authorizationRequestResolver;
+    }
+
+    private Consumer<OAuth2AuthorizationRequest.Builder>
+    authorizationRequestCustomizer() {
+        return customizer -> customizer
+                .additionalParameters(params -> params.put("audience", audience));
+    }
+}
---
+++ gateway/src/main/java/com/example/gateway/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package com.example.gateway.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView home(@AuthenticationPrincipal OidcUser user) {
+        return new ModelAndView("home", Collections.singletonMap("claims", user.getClaims()));
+    }
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String admin(@AuthenticationPrincipal OidcUser user) {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+
+        return "Hello, Admin!<br/><br/>User: " + user.getFullName() + "!<br/><br/>Authorities: " + authorities;
+    }
+}
