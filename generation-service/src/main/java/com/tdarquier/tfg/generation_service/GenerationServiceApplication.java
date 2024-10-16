caeef8fe7a795c408797e8dcdbcacdbb12901874
tomasdarquier <tomasdarquier@gmail.com>
2024-10-16 19:10:04 -0300

    inicializacion de generation-service y gestion de cliente feign

---
+++ generation-service/src/main/java/com/tdarquier/tfg/generation_service/GenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tdarquier.tfg.generation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationServiceApplication.class, args);
+	}
+
+}
---
+++ generation-service/src/test/java/com/tdarquier/tfg/generation_service/GenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdarquier.tfg.generation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
